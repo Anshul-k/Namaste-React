3a1c97b51b93176e2418401b5334ebf756f4263d
Kasana <anshul.kasana@accenture.com>
2024-07-01 21:20:53 +0530

    Namaste React Episode 2 - Igniting Our App

---
+++ Episode_2- Igniting_our_app/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an h1 Tag"),
+    React.createElement("h2", {}, "I am an h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 Tag"),
+    React.createElement("h2", {}, "I am an h2 Tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyx: "abc" },
+  "Hello world from React !!"
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ Episode_2- Igniting_our_app/Readme.md
@@ -0,0 +1,19 @@
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement 
+- File Watchig Alogrithm
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compress 
+- Consistent Hashing
+- Code Spiltting 
+- Differnential Bundling - Support Older Browsers
+- Diagnostics
+- Error Handling
+- HTTPS
+- Tree Shaking - Remove unused code
+- Different Dev and Prod Bundles 
